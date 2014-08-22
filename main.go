0037edc7fee1f2f9fc1d9a7607ef4c1ed049673c
Guilhem Lettron <guilhem.lettron@optiflows.com>
2014-08-22 14:45:09 +0200

    Use globalconf

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
-	"github.com/ant0ine/go-json-rest/rest"
+	"flag"
 	"log"
 	"net"
 	"net/http"
 	"os"
 	"os/signal"
+	"os/user"
+	"strconv"
 	"syscall"
+
+	"github.com/ant0ine/go-json-rest/rest"
+	"github.com/rakyll/globalconf"
+)
+
+var (
+	flagSocket = flag.String("socket", "/var/run/surysys", "Path for unix socket")
+	flagOwner  = flag.String("owner", "", "Ownership for socket")
+	// flagMode   = flag.Int("mode", 0640, "FileMode for socket")
 )
 
 type Message struct {
@@ -15,8 +26,14 @@ type Message struct {
 }
 
 func main() {
+	conf, err := globalconf.New("myapp")
+	if err != nil {
+		log.Fatal(err)
+	}
+	conf.ParseAll()
+
 	handler := rest.ResourceHandler{}
-	err := handler.SetRoutes(
+	err = handler.SetRoutes(
 		&rest.Route{"GET", "/message", func(w rest.ResponseWriter, req *rest.Request) {
 			w.WriteJson(&Message{
 				Body: "Hello World!",
@@ -26,23 +43,39 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	ln, err := net.Listen("unix", "/tmp/lol")
+	ln, err := net.Listen("unix", *flagSocket)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	if *flagOwner != "" {
+		user, err := user.Lookup(*flagOwner)
+		if err != nil {
+			log.Fatal(err)
+		}
+		uid, err := strconv.Atoi(user.Uid)
+		gid, err := strconv.Atoi(user.Gid)
+		if err := os.Chown(*flagSocket, uid, gid); err != nil {
+			log.Fatal(err)
+		}
+	}
+	// if err := os.Chmod(*flagSocket, *flagMode); err != nil {
+	// 	log.Fatal(err)
+	// }
+
 	// Unix sockets must be unlink()ed before being reused again.
 
 	// Handle common process-killing signals so we can gracefully shut down:
 	sigc := make(chan os.Signal, 1)
 	signal.Notify(sigc, os.Interrupt, os.Kill, syscall.SIGTERM)
 	go func(c chan os.Signal) {
-	    // Wait for a SIGINT or SIGKILL:
-	    sig := <-c
-	    log.Printf("Caught signal %s: shutting down.", sig)
-	    // Stop listening (and unlink the socket if unix type):
-	    ln.Close()
-	    // And we're done:
-	    os.Exit(0)
+		// Wait for a SIGINT or SIGKILL:
+		sig := <-c
+		log.Printf("Caught signal %s: shutting down.", sig)
+		// Stop listening (and unlink the socket if unix type):
+		ln.Close()
+		// And we're done:
+		os.Exit(0)
 	}(sigc)
 
 	log.Fatal(http.Serve(ln, &handler))
